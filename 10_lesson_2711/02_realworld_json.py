7dfe535a7c4019677ee102ec1b4f8bf9fc6f3305
demidraksin <greatraksin@icloud.com>
2021-11-27 16:27:52 +0300

    Adding JSON lesson

--- 10_lesson_2711/02_realworld_json.py
+++ 10_lesson_2711/02_realworld_json.py
@@ -1,3 +1,12 @@
+import json
+import requests
 
 
 url = 'https://jsonplaceholder.typicode.com/todos'
+response = requests.get(url)  # подключаюсь к сайту в переменной url
+todo = json.loads(response.text)
+# ^ превращаю ответ сервера в json-файл
+for smth in todo:
+    if (smth['userId'] == 4) and (not smth['completed']):
+        print(smth)
+"""Выводить задачи пользователя с ID=4, которые он не выполнил"""
