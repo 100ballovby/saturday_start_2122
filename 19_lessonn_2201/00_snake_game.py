46f39616f170e7b9f8aebad60c2a1c42ed3949e1
demidraksin <greatraksin@icloud.com>
2022-01-22 15:19:15 +0300

    Adding key listener ⃣

--- 19_lessonn_2201/00_snake_game.py
+++ 19_lessonn_2201/00_snake_game.py
@@ -13,6 +13,10 @@ screen = pg.display.set_mode((S_WIDTH, S_HEIGHT))
 pg.display.set_caption('Snake Game v.1')  # название окна
 clock = pg.time.Clock()
 
+x_change = 0  # то, насколько изменяются координаты змеи
+y_change = 0  # то, насколько изменяются координаты змеи
+x1 = 200  # начальные координаты змейки
+y1 = 200  # начальные координаты змейки
 
 pg.display.update()
 while not done:
@@ -20,5 +24,21 @@ while not done:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             done = True
+        elif event.type == pg.KEYDOWN:  # если нажали на какую-то кнопку
+            if event.key == pg.K_LEFT:  # если кнопка - стрелка_влево, то
+                x_change = -10  # идем влево (уменьшаем х)
+                y_change = 0
+            elif event.key == pg.K_RIGHT:  # если кнопка - стрелка_вправо, то
+                x_change = 10  # идем вправо (увеличиваем х)
+                y_change = 0
+            elif event.key == pg.K_UP:  # если кнопка - стрелка_вверх, то
+                x_change = 0
+                y_change = 10  # идем вверх (увеличиваем у)
+            elif event.key == pg.K_DOWN:  # если кнопка - стрелка_вниз, то
+                x_change = 0
+                y_change = -10  # идем вниз (уменьшаем у)
+
+    rect(screen, GREEN, [200, 150, 10, 10])
+    pg.display.update()
 
 
