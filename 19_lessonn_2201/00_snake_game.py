de7974959c62d54b54548c35a7fe6c6df9543642
demidraksin <greatraksin@icloud.com>
2022-01-22 15:50:05 +0300

    Finishing snake movement and adding show_message function 🥲

--- 19_lessonn_2201/00_snake_game.py
+++ 19_lessonn_2201/00_snake_game.py
@@ -4,8 +4,18 @@ from pygame.draw import rect
 BLUE = (120, 84, 240)
 RED = (240, 84, 84)
 GREEN = (74, 224, 74)
+VIOLET = (248, 240, 255)
 S_WIDTH = 640
 S_HEIGHT = 480
+SPEED = 5
+
+
+def show_message(msg, color, surface):
+    font_style = pg.font.SysFont(bold=True, size=30)  # стиль шрифта - жирный, размер 30
+    text = font_style.render(msg, True, color)  # отобразить сообщение
+    surface.blit(text, [S_WIDTH / 2, S_HEIGHT / 2])
+
+
 
 done = False
 
@@ -26,19 +36,34 @@ while not done:
             done = True
         elif event.type == pg.KEYDOWN:  # если нажали на какую-то кнопку
             if event.key == pg.K_LEFT:  # если кнопка - стрелка_влево, то
-                x_change = -10  # идем влево (уменьшаем х)
+                x_change = SPEED * -1  # идем влево (уменьшаем х)
                 y_change = 0
             elif event.key == pg.K_RIGHT:  # если кнопка - стрелка_вправо, то
-                x_change = 10  # идем вправо (увеличиваем х)
+                x_change = SPEED  # идем вправо (увеличиваем х)
                 y_change = 0
             elif event.key == pg.K_UP:  # если кнопка - стрелка_вверх, то
                 x_change = 0
-                y_change = 10  # идем вверх (увеличиваем у)
+                y_change = SPEED * -1  # идем вверх (уменьшаем у)
             elif event.key == pg.K_DOWN:  # если кнопка - стрелка_вниз, то
                 x_change = 0
-                y_change = -10  # идем вниз (уменьшаем у)
+                y_change = SPEED  # идем вниз (увеличиваем у)
 
-    rect(screen, GREEN, [200, 150, 10, 10])
+    x1 += x_change  # заставляю змею двигаться по иксу
+    y1 += y_change  # заставляю змею двигаться по игреку
+
+    if x1 > S_WIDTH:
+        x1 *= 0
+    elif x1 <= 0:
+        x1 = S_WIDTH - 10
+    elif y1 > S_HEIGHT:
+        y1 *= 0
+    elif y1 <= 0:
+        y1 = S_HEIGHT - 10
+
+    screen.fill(VIOLET)
+    rect(screen, GREEN, [x1, y1, 10, 10])
     pg.display.update()
+
+    # TODO: сделать змею другого цвета, если коснется объекта
 
 
