19a6f954681839684a90be9641b49e5d069b4b91
demidraksin <greatraksin@icloud.com>
2022-02-26 14:56:23 +0300

    Adding game variables

--- 22_lesson_2602/00_homework.py
+++ 22_lesson_2602/00_homework.py
@@ -1,8 +1,9 @@
 import pygame as pg
 from pygame.draw import rect, circle, polygon
+from random import randint
 
-W = 640
-H = 640
+W = 1280
+H = 720
 screen = pg.display.set_mode((W, H))
 clock = pg.time.Clock()
 
@@ -10,6 +11,23 @@ WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 BLACK = (0, 0, 0)
 
+# Game Variables
+speed = 15  # отвечает за скорость передвижения платформы и шарика
+pl_x = W // 2
+pl_y = H - 100
+
+rad = 40
+cir_x = randint(0 + rad, W - rad)
+cir_y = 0 - rad
+
+score = 3
+
+
+def show_text(text, x, y):
+    pg.font.init()
+    font = pg.font.SysFont('comicsans', 32)  # настройки шрифта
+    msg = font.render(text, True, BLACK)
+    screen.blit(msg, x, y)
 
 finished = False
 while not finished:
