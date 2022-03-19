9c7831e278f535c21beb019e57c1eb5d8e51cdc0
demidraksin <greatraksin@icloud.com>
2022-03-19 15:49:47 +0300

    Adding the second paddle

--- 25_lesson_1903/00_racing.py
+++ 25_lesson_1903/00_racing.py
@@ -1,5 +1,6 @@
 import pygame as pg
 from pygame.draw import rect, circle, polygon
+import random as r
 
 W = 800
 H = 900
@@ -26,7 +27,8 @@ road = pg.Rect(0, 0, W / 2, H)  # дорога
 road.center = W // 2, H // 2  # выставляю дорогу посередине
 border_left = pg.Rect(road.x - W * 0.12, 0, W * 0.12, H)  # левая обочина
 border_right = pg.Rect(road.right, 0, W * 0.12, H)  # правая обочина
-paddle1 = pg.Rect(170, 30, 100, 20)  # препятствие
+paddle1 = pg.Rect(road.x, 0, 100, 20)  # препятствие №1
+paddle2 = pg.Rect(road.x, -500, 100, 20)  # препятствие №2
 
 
 def moving(obj, speed, l_side, r_side):
@@ -70,14 +72,33 @@ while not finished:
     rect(screen, SAND, border_left)  # левая обочина
     rect(screen, SAND, border_right)  # правая обочина
     rect(screen, WHITE, [W // 2, 0, 5, H])  # полоса разметки
-
-    rect(screen, RED, paddle1)  # препятствие
+    rect(screen, RED, paddle1)  # препятствие №1
+    rect(screen, RED, paddle2)  # препятствие №2
 
     screen.blit(img_copy, img_rect_copy)
     pg.display.update()
 
     # Логика игры
     moving(img_rect, car_speed, border_left, border_right)
+    num = r.randint(1, 3)  # генерирую число от 1 до 3
     paddle1.y += 10
+    paddle2.y += 10
+
     if paddle1.y >= H:
+        if num == 1:
+            paddle1.x = road.x
+        elif num == 2:
+            paddle1.center = road.center
+        else:
+            paddle1.right = road.right
         paddle1.y = 0
+
+    num = r.randint(1, 3)  # генерирую число от 1 до 3
+    if paddle2.y >= H:
+        if num == 1:
+            paddle2.x = road.x
+        elif num == 2:
+            paddle2.center = road.center
+        else:
+            paddle2.right = road.right
+        paddle2.y = 0
