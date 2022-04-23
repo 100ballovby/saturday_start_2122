edae7cb0ddac0e34fa7b81145fe8e113e8d3bc1f
demidraksin <greatraksin@icloud.com>
2022-04-23 14:36:08 +0300

    Adding API lesson

--- 29_lesson_1604/01_timeseries.py
+++ 29_lesson_1604/01_timeseries.py
@@ -2,16 +2,16 @@ import requests as r
 import json
 
 base_currency = 'USD'
-symbols = 'NG'  # XAU - золото, SUGAR - сахар, COTTON - хлопок, NG - газ
+symbols = 'CORN'  # XAU - золото, SUGAR - сахар, COTTON - хлопок, NG - газ
 endpoint = 'timeseries'
 key = ''   # вставьте ваш ключ к API
-start = '2021-04-15'
-end = '2022-04-15'
+start = '2021-04-23'
+end = '2022-04-23'
 url = f'https://commodities-api.com/api/{endpoint}?access_key={key}&start_date={start}&end_date={end}&symbols={symbols}'
 
 
 resp = r.get(url)
-with open('series_NG.json', 'w') as file:
+with open('series_CORN.json', 'w') as file:
     res = resp.text
     file.write(res)
 
